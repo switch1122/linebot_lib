17a936d9d3dfb24d01e6810f0f4542bee8c7ddd6
switch1122 <46814343+switch1122@users.noreply.github.com>
2019-01-24 13:16:52 +0900

    Update define.php

--- define.php
+++ define.php
@@ -1,4 +1,5 @@
 <?php
-define('ACCESS_TOKEN','');  //アクセストークン
-define('CHANNEL_SECRET','');  //シークレット
+define('ACCESS_TOKEN','');  //Kwlg7zEwOmGPsUO29PGu8RZ7VFWhu4S9uSK5Dt/wRg87FFRU7iPsji63B0lagCEU2nb2Vj8d78hbzFEI8t4Leer3Fn0AKqg/jRcoNw1kP5Z3Tgn/syplPK9wEEzku+Rm8rxOdmSwNQwmExEt90MIfwdB04t89/1O/w1cDnyilFU=
+define('CHANNEL_SECRET','');  //625a6bcb740d2407939a8979af56ac93
+
 ?>
